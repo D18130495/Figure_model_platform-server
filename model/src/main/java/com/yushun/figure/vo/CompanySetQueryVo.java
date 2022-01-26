6b007dd730c43703a9169b1dd3f0ef4931f30ec5
D18130495 <D18130495@mytudublin.ie>
2022-01-26 22:00:47 +0000

    add some company api

---
+++ model/src/main/java/com/yushun/figure/vo/CompanySetQueryVo.java
@@ -0,0 +1,22 @@
+package com.yushun.figure.vo;
+
+public class CompanySetQueryVo {
+    private String compName;
+    private String compCode;
+
+    public String getCompName() {
+        return compName;
+    }
+
+    public void setCompName(String compName) {
+        this.compName = compName;
+    }
+
+    public String getCompCode() {
+        return compCode;
+    }
+
+    public void setCompCode(String compCode) {
+        this.compCode = compCode;
+    }
+}
--- service/service_company/src/main/java/com/yushun/figure/company/controller/CompanySetController.java
+++ service/service_company/src/main/java/com/yushun/figure/company/controller/CompanySetController.java
@@ -1,12 +1,18 @@
 package com.yushun.figure.company.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yushun.figure.common.result.Result;
+import com.yushun.figure.common.utils.MD5;
 import com.yushun.figure.company.service.CompanySetService;
 import com.yushun.figure.model.company.CompanySet;
-import lombok.val;
+import com.yushun.figure.vo.CompanySetQueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/admin/comp/companySet")
@@ -18,15 +24,60 @@ public class CompanySetController {
     //http://localhost:8021/admin/comp/companySet/findAll
     // get all the company information
     @GetMapping("findAll")
-    public List<CompanySet> findAllCompanySet() {
+    public Result findAllCompanySet() {
         List<CompanySet> list = companySetService.list();
-        return list;
+        return Result.ok(list);
+    }
+
+    // get company information pagination
+    @GetMapping("findPage/{current}/{limit}")
+    public Result findPaginationCompanySet(@PathVariable long current,
+                                           @PathVariable long limit,
+                                           @RequestBody(required = false) CompanySetQueryVo companySetQueryVo) {
+        Page<CompanySet> page = new Page<>(current, limit);
+        QueryWrapper<CompanySet> wrapper = new QueryWrapper<>();
+
+        String compName = companySetQueryVo.getCompName();
+        String compCode = companySetQueryVo.getCompCode();
+
+        if(!StringUtils.isEmpty(compName)) {
+            wrapper.like("comp_name", compName);
+        }
+
+        if(!StringUtils.isEmpty(compCode)) {
+            wrapper.eq("comp_code", compCode);
+        }
+
+        Page<CompanySet> pageCompanySet = companySetService.page(page, wrapper);
+        return Result.ok(pageCompanySet);
     }
 
     // delete the company(logic)
     @DeleteMapping("{id}")
-    public boolean removeCompanySet(@PathVariable Long id) {
+    public Result removeCompanySet(@PathVariable long id) {
         Boolean flag = companySetService.removeById(id);
-        return flag;
+
+        if(flag) {
+            return  Result.ok();
+        }else {
+            return Result.fail();
+        }
+    }
+
+    // add company set
+    @PostMapping("saveCompanySet")
+    public Result addCompanySet(@RequestBody CompanySet companySet) {
+        // status 1 can be use, status can not be use
+        companySet.setStatus(1);
+        // sign key
+        Random random = new Random();
+        companySet.setSignKey(MD5.encrypt(System.currentTimeMillis() + "" + random.nextInt(1000)));
+
+        Boolean flag = companySetService.save(companySet);
+        if(flag) {
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
     }
 }
