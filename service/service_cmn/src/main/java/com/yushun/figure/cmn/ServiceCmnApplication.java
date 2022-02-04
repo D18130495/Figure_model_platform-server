7bbc728b617a9ef91592f93dc0ddda66191cfbc2
D18130495 <D18130495@mytudublin.ie>
2022-02-04 23:23:28 +0000

    Add nacos

--- service/service_cmn/src/main/java/com/yushun/figure/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/yushun/figure/cmn/ServiceCmnApplication.java
@@ -2,10 +2,12 @@ package com.yushun.figure.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.yushun")
+@EnableDiscoveryClient
 public class ServiceCmnApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCmnApplication.class, args);
--- service/service_company/src/main/java/com/yushun/figure/company/ServiceCompanyApplication.java
+++ service/service_company/src/main/java/com/yushun/figure/company/ServiceCompanyApplication.java
@@ -2,10 +2,12 @@ package com.yushun.figure.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.yushun")
+@EnableDiscoveryClient
 public class ServiceCompanyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCompanyApplication.class, args);
